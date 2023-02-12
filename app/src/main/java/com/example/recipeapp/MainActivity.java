7ce1dc49ebecfa256f357af30cdd1b1a9e97627f
Muqadas Iqbal <muqadasiqbal807@gmail.com>
2023-02-12 15:45:30 +0500

    PinView

---
+++ app/src/main/java/com/example/recipeapp/MainActivity.java
@@ -0,0 +1,156 @@
+package com.example.recipeapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.goodiebag.pinview.Pinview;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mRecyclerView;
+    List<FoodData> myFoodList;
+    FoodData mFoodData;
+    ProgressDialog progressDialog;
+    MyAdapter myAdapter;
+    EditText txt_Search;
+    String fileName;
+    String pinCode;
+    Dialog dialog;
+    Pinview pinview;
+    Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(MainActivity.this,1);
+        mRecyclerView.setLayoutManager(gridLayoutManager);
+
+        txt_Search = (EditText)findViewById(R.id.txt_searchtext);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("Loading Items ....");
+
+        myFoodList = new ArrayList<>();
+
+        myAdapter  = new MyAdapter(MainActivity.this,myFoodList);
+        mRecyclerView.setAdapter(myAdapter);
+
+
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("Recipe");
+
+        progressDialog.show();
+        ValueEventListener eventListener = databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                myFoodList.clear();
+
+                for (DataSnapshot itemSnapshot : dataSnapshot.getChildren()) {
+
+                    FoodData foodData = itemSnapshot.getValue(FoodData.class);
+                    foodData.setKey(itemSnapshot.getKey());
+                    myFoodList.add(foodData);
+
+                }
+
+                myAdapter.notifyDataSetChanged();
+                progressDialog.dismiss();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                progressDialog.dismiss();
+            }
+        });
+
+
+        txt_Search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+                filter(s.toString());
+
+            }
+        });
+
+
+
+    }
+    private void filter(String text) {
+
+        ArrayList<FoodData> filterList = new ArrayList<>();
+
+        for(FoodData item: myFoodList){
+
+            if(item.getItemName().toLowerCase().contains(text.toLowerCase())){
+
+                filterList.add(item);
+
+            }
+
+        }
+
+        myAdapter.filteredList(filterList);
+
+    }
+    public void btn_uploadActivity(View view) {
+        dialog=new Dialog(MainActivity.this);
+        dialog.setContentView(R.layout.dialogdesign);
+        pinview =dialog.findViewById(R.id.etPinCode);
+         button=dialog.findViewById(R.id.sendbtn);
+        dialog.show();
+
+        button .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SimpleDateFormat format=new SimpleDateFormat("HHmm", Locale.CANADA);
+                Date now=new Date();
+                fileName=format.format(now);
+                pinCode=pinview.getValue().toString();
+                if (pinCode.equals(fileName)) {
+                    startActivity(new Intent(MainActivity.this,Upload_Recipe.class));
+                    dialog.dismiss();
+                } else {
+                    Toast.makeText(MainActivity.this, "wrong pin code", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
