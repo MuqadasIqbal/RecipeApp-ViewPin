f69c64ce380102ebef9eb627ab63d2311131c4f2
Muqadas Iqbal <muqadasiqbal807@gmail.com>
2023-02-14 19:03:31 +0500

    cameraImage

--- app/src/main/java/com/example/recipeapp/Upload_Recipe.java
+++ app/src/main/java/com/example/recipeapp/Upload_Recipe.java
@@ -4,10 +4,14 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -22,14 +26,19 @@ import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
+import java.io.ByteArrayOutputStream;
 import java.text.DateFormat;
 import java.util.Calendar;
 
+@SuppressWarnings("ALL")
 public class Upload_Recipe extends AppCompatActivity {
-    ImageView recipeImage;
+    ImageView recipeImage,cameraImage,galleryImage;
     Uri uri;
     EditText txt_name,txt_description,txt_price;
     String imageUrl;
+    Dialog dialog;
+    private final int CAMERA_REQUEST_CODE=100;
+    private final int GALLERY_CODE=200;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,27 +49,59 @@ public class Upload_Recipe extends AppCompatActivity {
         txt_price = (EditText)findViewById(R.id.text_price);
 
     }
-
     public void btnSelectImage(View view) {
+        dialog=new Dialog(Upload_Recipe.this);
+        dialog.setContentView(R.layout.selectimage);
+        cameraImage=dialog.findViewById(R.id.camera);
+        galleryImage=dialog.findViewById(R.id.gallary);
+        dialog.show();
 
-        Intent photoPicker = new Intent(Intent.ACTION_PICK);
-        photoPicker.setType("image/*");
-        startActivityForResult(photoPicker,1);
+        cameraImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             Intent intent=new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+             startActivityForResult(intent,CAMERA_REQUEST_CODE);
+             dialog.dismiss();
+            }
+        });
+        galleryImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent photoPicker = new Intent(Intent.ACTION_PICK);
+                photoPicker.setType("image/*");
+                startActivityForResult(photoPicker,1);
+                dialog.dismiss();
+            }
+        });
+    }public Uri getImageUri(Context inContext, Bitmap inImage){
+        ByteArrayOutputStream bytes=new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        String path = MediaStore.Images.Media.insertImage(inContext.getContentResolver(), inImage, "Title", null);
+        return Uri.parse(path);
     }
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
         if(resultCode == RESULT_OK){
+         if (requestCode==CAMERA_REQUEST_CODE){
+             Bitmap urii=(Bitmap) (data.getExtras().get("data"));
+             uri=getImageUri(Upload_Recipe.this,urii);
+             recipeImage.setImageURI(uri);
+         }
+         else{
+             uri = data.getData();
+             recipeImage.setImageURI(uri);
+         }
 
-            uri = data.getData();
-            recipeImage.setImageURI(uri);
 
         }
         else Toast.makeText(this, "You haven't picked image", Toast.LENGTH_SHORT).show();
 
     }
+
 
     public void uploadImage(){
 
